096d21abae95de4c5dd56187610112b8b7aaafaf
Kevin Lacker <lacker@gmail.com>
2020-10-29 08:17:57 -0700

    reading headers fro mtar

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("hello TypeScript world");
+import fs from "fs";
+import tar from "tar-stream";
+
+let extract = tar.extract();
+extract.on("entry", (header, stream, next) => {
+  console.log("header:", header);
+
+  //
+  stream.on("end", next);
+  stream.resume();
+});
+
+extract.on("finish", () => {
+  console.log("finished");
+});
+
+fs.createReadStream("/d/pile/github.tar").pipe(extract);
