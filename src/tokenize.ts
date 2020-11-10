463bbbf37e08aec16f182250eccbfe787beaf9b8
Kevin Lacker <lacker@gmail.com>
2020-11-10 10:19:13 -0800

    add a couint

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -17,18 +17,20 @@ let INDEX_FILE = "/d/tokens/index.json";
 let indexList = [];
 // token -> id
 let indexMap = {};
+// token -> count
+let count = {};
 let maxFile = 0;
 try {
   let raw = fs.readFileSync(INDEX_FILE, "utf8");
   let data = JSON.parse(raw);
-  ({ indexList, indexMap, maxFile } = data);
+  ({ indexList, indexMap, maxFile, count } = data);
 } catch (e) {
   console.log(e);
 }
 
 // Save the index
 function save() {
-  let data = { indexList, indexMap, maxFile };
+  let data = { indexList, indexMap, maxFile, count };
   let raw = JSON.stringify(data, null, 2);
   fs.writeFileSync(INDEX_FILE, raw);
 }
